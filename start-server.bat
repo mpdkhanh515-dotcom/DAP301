bc94c24b30e0e3473f6dbe18be4271864021305b
mpdkhanh515-dotcom <mpdkhanh515@gmail.com>
2025-11-03 09:48:25 +0700

    Add files via upload

---
+++ start-server.bat
@@ -0,0 +1,10 @@
+PK    ) [[               start-server.bat@echo off
+echo Starting local web server...
+echo.
+echo Open your browser to: http://localhost:8000/index.html
+echo.
+echo Press Ctrl+C to stop the server
+echo.
+python -m http.server 8000
+
+PK ;ŸþÑ   Ñ   PK-    ) [[;ŸþÑ   Ñ              ¤    start-server.batPK      >       
---
+++ start-server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Starting local web server..."
+echo ""
+echo "Open your browser to: http://localhost:8000/index.html"
+echo ""
+echo "Press Ctrl+C to stop the server"
+echo ""
+python3 -m http.server 8000
+
